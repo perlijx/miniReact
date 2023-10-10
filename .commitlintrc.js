44e0c9af872db3ed41e9d4444ec2bc8156303a36
perli <1003914407@qq.com>
2023-10-10 18:18:00 +0800

    feat: 初始化项目

---
+++ .commitlintrc.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: perli 1003914407@qq.com
+ * @Date: 2023-10-10 17:24:06
+ * @LastEditors: perli 1003914407@qq.com
+ * @LastEditTime: 2023-10-10 17:24:31
+ * @FilePath: /React/.commitlintrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+	extends: ['@commitlint/config-conventional']
+};
